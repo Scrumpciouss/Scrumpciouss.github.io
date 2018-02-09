208267d0ae65c340ff44d0e306f4a534dfcccbdc
Scrumpciouss <phillips017@gmail.com>
2018-02-09 14:29:03 -0600

    updated

---
+++ interactions/sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  document.addEventListener("contextmenu", event => event.preventDefault())
+}
+
+
+
+
+// function keyPressed() {
+//   fill(random(255), random(255), random(255), random(255));
+//   ellipse(random(0, width), random(0, height), random(100), random(100));
+// }
+
+//noStroke()
+// function mouseClicked() {
+//   fill(random(255), random(255), random(255), random(255))
+//   rect(mouseX, mouseY, (50, 200), random(0, 200));
+// }
+
+function deviceShaken() {
+  fill(2);
+  textFont("Arial");
+  textAlign(CENTER);
+  textSize(32);
+  text("shaken, not stirred.", width / 2, height / 2);
+}
+
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    fill(random(255), random(255), random(255));
+    rect(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+  else if (mouseButton === RIGHT){
+    ellipse(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+}
+
+function keypressed(){
+  if (key === 'w' || key ==='W') {}
+    background(0);
+}
