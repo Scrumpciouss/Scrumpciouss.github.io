0840e0373bf97072364c0332ff8f88b001df6881
Scrumpciouss <phillips017@gmail.com>
2018-02-16 14:37:33 -0600

    added movement

--- better interactions/script.js
+++ better interactions/script.js
@@ -1,8 +1,12 @@
 // globqal variables
-let x;
-let y;
 let isMovingUp, isMoveingDown, isMovingleft, isMovingRight;
-let redAmount, redSpeed ;
+let redAmount;
+let gravity;
+let x, y, radius;
+
+
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,37 +17,49 @@ function setup() {
   isMoveingDown = false;
   isMovingRight = false;
   redAmount = 0;
+  x = width / 2;
+  y = height / 2;
+  radius = 50;
+
 }
 
 function draw() {
   background(255);
 
-  moveStickman();
-
-  drawStickman(mouseX, mouseY);
-  drawStickman(x, y);
-}
-
-function moveStickman() {
-  if (isMovingUp) {
-    y -= 10;
-  }
 
 }
+
+// function moveStickman() {
+//   if (isMovingUp) {
+//     y -= 10;
+//   }
+//   else (isMovingleft) {
+//     x -= 10;
+//   }
+//   if (isMovingRight){
+//     x += 10;
+//   }
+//   else (isMoveingDown) {
+//     y =+ 10;
+//   }
+//
+// }
 
 function keyPressed() {
 
   if (key === "w" || key === "W") {
     //
     isMovingUp = true;
-  } else if (key === "a" || key === "A") {
+  }
+  else if (key === "a" || key === "A") {
     //
     isMovingleft = true
   }
   if (key === "s" || key === "S") {
     //
     isMoveingDown = true
-  } else if (key === "d" || key === "D") {
+  }
+  else if (key === "d" || key === "D") {
     //
     isMovingRight = true
   }
@@ -54,47 +70,49 @@ function keyReleased() {
   if (key === "w" || key == "W") {
     //
     isMovingUp = false;
-  } else if (key === "a" || key == "A") {
+  }
+  else if (key === "a" || key == "A") {
     //
     isMovingleft = false
   }
   if (key === "s" || key == "S") {
     //
     isMoveingDown = false
-  } else if (key === "d" || key == "D") {
+  }
+  else if (key === "d" || key == "D") {
     //
     isMovingRight = false
   }
 }
 
 function drawStickman(x, y) {
-// constrain to left side of screen
-x = constrain(x,0,width/2);
+  // constrain to left side of screen
+  x = constrain(x, 0, width / 2);
 
   //body
 
-  line(x, y, x, y + 200);
+  line(x, y, x, y + 100);
 
   // draw head of stickman
   fill(255, 255, 255);
-  ellipse(x, y, 100, 100);
+  ellipse(x, y, 50, 50);
   // hat
-  redAmount = (redAmount + 1 ) % 255
+  redAmount = (redAmount + 1) % 255
   redAmount += 1;
   redAmount = constrain(redAmount, 0, 255);
   if (redAmount === 255) {
     redAmount = 0
   }
   fill(redAmount, 0, 0)
-  rect(x - 50, y - 80, 100, 30);
-  rect(x - 25, y - 130, 50, 50);
+  rect(x - 50, y - 50, 100, 30);
+  rect(x - 25, y - 80, 50, 50);
 
   // arms
-  line(x - 50, y + 100, x + 50, y + 100);
+  line(x - 50, y + 50, x + 50, y + 50);
 
   // Legs\
-  line(x, y + 200, x - 50, y + 250);
-  line(x, y + 200, x + 50, y + 250);
+  line(x, y + 100, x - 50, y + 150);
+  line(x, y + 100, x + 50, y + 150);
 
 
 }
