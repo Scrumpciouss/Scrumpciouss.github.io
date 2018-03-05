79b9e2ef99f55ff64d8e3949fb246c0ca97bb3a1
Scrumpciouss <phillips017@gmail.com>
2018-03-05 15:20:03 -0600

    asdf

--- obj/sketch.js
+++ obj/sketch.js
@@ -1,34 +1,24 @@
-// p5js template project - replace with project title
+// obj demo
 // Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// March 5  2018 - replace with the date
 
-// global variables
-let gear;
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
+let ball = {
+  x: 200,
+  y: 300,
+  radius: 50,
+};
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
+  fill(0);
 
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+  // follow mouse
+  ball.x = mouseX;
+  ball.y = mouseY;
+  
+  ellipse(ball.x, ball.y, ball.radius, ball.radius)
 }
