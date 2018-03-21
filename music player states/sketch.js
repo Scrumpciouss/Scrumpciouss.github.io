f438fd108572b1cc55eafec98a13061375b119ab
Scrumpciouss <36087435+Scrumpciouss@users.noreply.github.com>
2018-03-21 17:36:07 -0600

    stuff

--- music player states/sketch.js
+++ music player states/sketch.js
@@ -3,21 +3,24 @@
 // Feb 15, 2018
 
 // global variables
-let backgroundMusic
+let backgroundMusic;
 let state;
 // let characterImage;
 
-function preload() {
-  // characterImage = loadImage("images/nameOfFile.png");
-}
+// function preload() {
+//   // characterImage = loadImage("images/nameOfFile.png");
+//   bakcgroundMusic = loadSound("assests/Heroic Demise (New).mp3");
+//   backgroundMusic = loadSound("assests/Rise of spirit.mp3");
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
-  radius = 50;
-  dx = random(2, 5);
-  dy = random(2, 5);
+  // x = width / 2;
+  // y = height / 2;
+  // backgroundMusic.setVolume(0.6);
+  // backgroundMusic.loop();
+
+
 
   state = 1;
 }
@@ -28,14 +31,14 @@ function draw() {
     displayStartScreen();
   }
   if (state === 2) {
-    loadMusicSelect1();
-    loadMusicSelect2();
-    loadMusicSelect3();
-    loadMusicSelect4();
+    // loadMusicSelect1();
+    // loadMusicSelect2();
+    // loadMusicSelect3();
+    // loadMusicSelect4();
   }
   // image(characterImage, 0, 0);
 }
-
+// main button to start program
 function displayStartScreen() {
   let buttonWidth = 400;
   let buttonHeight = 200;
@@ -56,7 +59,7 @@ function displayStartScreen() {
 }
 
 function loadMusicSelect1() {
-  let buttonWidth = 400;
+  let buttonWidth = 300;
   let buttonHeight = 200;
   let leftSide = width / 2 - buttonWidth / 2;
   let topSide = height / 2 - buttonHeight / 2;
@@ -64,5 +67,10 @@ function loadMusicSelect1() {
   let bottomSide = topSide + buttonHeight;
   fill(0);
   if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    // backgroundMusic.play();
     fill(125);
+  
+
+
+  }
 }
